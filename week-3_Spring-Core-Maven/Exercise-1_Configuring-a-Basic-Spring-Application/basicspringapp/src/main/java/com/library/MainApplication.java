2ca01e408376cb1fd1e19f99852dc1ef5777be15
sirimavilla kaushik <152891994+Sirimavilla-Kaushik@users.noreply.github.com>
2025-07-03 22:52:01 +0530

    Update MainApplication.java

--- week-3_Spring-Core-Maven/Exercise-1_Configuring-a-Basic-Spring-Application/basicspringapp/src/main/java/com/library/MainApplication.java
+++ week-3_Spring-Core-Maven/Exercise-1_Configuring-a-Basic-Spring-Application/basicspringapp/src/main/java/com/library/MainApplication.java
@@ -1,18 +1,11 @@
 package com.library;
-
 import com.library.service.BookService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class MainApplication {
     public static void main(String[] args) {
-        // Load Spring context
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        // Get the BookService bean
         BookService bookService = context.getBean("bookService", BookService.class);
-
-        // Call method to test
         bookService.displayBooks();
     }
 }

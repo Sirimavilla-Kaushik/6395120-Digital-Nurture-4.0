998862d95db83bc63b35091d149f47d29a5f674b
sirimavilla kaushik <152891994+Sirimavilla-Kaushik@users.noreply.github.com>
2025-07-03 22:51:19 +0530

    Update BookService.java

--- week-3_Spring-Core-Maven/Exercise-1_Configuring-a-Basic-Spring-Application/basicspringapp/src/main/java/com/library/service/BookService.java
+++ week-3_Spring-Core-Maven/Exercise-1_Configuring-a-Basic-Spring-Application/basicspringapp/src/main/java/com/library/service/BookService.java
@@ -1,14 +1,10 @@
 package com.library.service;
 import com.library.repository.BookRepository;
-
 public class BookService {
     private BookRepository bookRepository;
-
-    // Setter Injection
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
-
     public void displayBooks() {
         System.out.println("Calling service to display books...");
         bookRepository.fetchBookDetails();

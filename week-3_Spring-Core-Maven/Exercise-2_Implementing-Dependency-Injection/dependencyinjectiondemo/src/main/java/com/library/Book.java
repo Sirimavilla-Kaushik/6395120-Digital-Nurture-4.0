b2e0266ba12db472ce44691a6747345c0fa177b3
Sirimavilla-Kaushik <sirimavillakaushik@gmail.com>
2025-07-04 22:19:48 +0530

    Successfully added

---
+++ week-3_Spring-Core-Maven/Exercise-2_Implementing-Dependency-Injection/dependencyinjectiondemo/src/main/java/com/library/Book.java
@@ -0,0 +1,10 @@
+package com.library;
+public class Book {
+    private String title;
+    public Book(String title) {
+        this.title = title;
+    }
+    public String getTitle() {
+        return title;
+    }
+}

cc5dfc5808897f501fa65ee5aad2d6f1fe3bc663
Sirimavilla-Kaushik <sirimavillakaushik@gmail.com>
2025-07-05 21:48:31 +0530

    Successfully added

--- week-3_Spring-Core-Maven/Exercise-2_Implementing-Dependency-Injection/dependencyinjectiondemo/src/main/java/com/library/LibraryManagementApplication.java
+++ week-3_Spring-Core-Maven/Exercise-2_Implementing-Dependency-Injection/dependencyinjectiondemo/src/main/java/com/library/LibraryManagementApplication.java
@@ -4,6 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class LibraryManagementApplication {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        
         BookService bookService = (BookService) context.getBean("bookService");
         bookService.addBook("Effective Java");
     }
